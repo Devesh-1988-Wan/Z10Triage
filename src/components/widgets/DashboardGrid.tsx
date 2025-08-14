2901dee3985c66e54239d0109ba585b310ec4457
Devesh-1988-Wan <devesh.pillewan@amla.io>
2025-08-14 23:47:49 +0530

    Update DashboardGrid.tsx

--- src/components/widgets/DashboardGrid.tsx
+++ src/components/widgets/DashboardGrid.tsx
@@ -1,10 +1,11 @@
-// src/components/DashboardGrid.tsx
+// src/components/widgets/DashboardGrid.tsx
 import React from 'react';
 import { Responsive, WidthProvider, Layout } from 'react-grid-layout';
 import { WidgetConfig } from '@/types/dashboard';
 import { WidgetRenderer } from '../WidgetRenderer';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import { ErrorBoundary } from './ErrorBoundary'; // Import the new ErrorBoundary component
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -13,6 +14,7 @@ interface DashboardGridProps {
   onLayoutChange: (layout: Layout[]) => void;
   isDraggable: boolean;
   data: any;
+  isLoading: boolean;
   onWidgetClick: (widget: WidgetConfig) => void;
   onUpdateWidget: (widget: WidgetConfig) => void;
 }
@@ -22,6 +24,7 @@ export const DashboardGrid: React.FC<DashboardGridProps> = ({
   onLayoutChange,
   isDraggable,
   data,
+  isLoading,
   onWidgetClick,
   onUpdateWidget
 }) => {
@@ -42,13 +45,16 @@ export const DashboardGrid: React.FC<DashboardGridProps> = ({
     >
       {layout.map((widget) => (
         <div key={widget.id}>
-          <WidgetRenderer
-            config={widget}
-            data={data}
-            onClick={() => onWidgetClick(widget)}
-            onUpdate={onUpdateWidget}
-            isEditable={isDraggable}
-          />
+          <ErrorBoundary>
+            <WidgetRenderer
+              config={widget}
+              data={data}
+              isLoading={isLoading}
+              onClick={() => onWidgetClick(widget)}
+              onUpdate={onUpdateWidget}
+              isEditable={isDraggable}
+            />
+          </ErrorBoundary>
         </div>
       ))}
     </ResponsiveGridLayout>
